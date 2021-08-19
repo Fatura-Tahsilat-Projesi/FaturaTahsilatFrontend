f19c8b898b6a22cf75c6a6fa2947f42519985f69
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2021-08-19 16:57:43 +0300

    projede düzenleme yapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopbarComponent } from '../modules/topbar/topbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { IconsModule } from '../modules/icons/icons.module';
-import { LayoutDashboardComponent } from '../modules/layout-dashboard/layout-dashboard.component';
-import { ModalComponent } from './modal/modal.component';
 
 const thirdParty = [IconsModule, NgbModule];
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent,
-    LayoutDashboardComponent,
-    ModalComponent,
   ],
   imports: [
     BrowserModule,

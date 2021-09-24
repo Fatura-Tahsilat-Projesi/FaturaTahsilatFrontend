4df499e594ab8d642be66b2e9bbc1050acc5dccf
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2021-09-24 11:06:34 +0300

    kredi kart düzenleme componenti eklendi.

--- src/models/creditcardmodel.ts
+++ src/models/creditcardmodel.ts
@@ -1,21 +1,21 @@
 export class CreditCardModel {
-    UserId: number | undefined;
+    UserId: string | undefined;
     cardNumber: string | undefined;
     ExpMonth: number | undefined;
     ExpYear: number | undefined;
     cvc2: number | undefined;
-    CreditCardType: number | undefined;
+    CreditCardType?: number | undefined;
     CreatedAt: Date | undefined;
     Balance: number | undefined;
     
 
-    constructor(UserId: number, cardNumber:string, ExpMonth: number, ExpYear:number, cvc2: number, CreditCardType: number, CreatedAt: Date, Balance: number){
+    constructor(UserId: string, cardNumber:string, ExpMonth: number, ExpYear:number, cvc2: number, CreatedAt: Date, Balance: number){
         this.UserId = UserId;
         this.cardNumber = cardNumber;
         this.ExpMonth = ExpMonth;
         this.ExpYear = ExpYear;
         this.cvc2 = cvc2;
-        this.CreditCardType = CreditCardType;
+        //this.CreditCardType = CreditCardType;
         this.CreatedAt = CreatedAt;
         this.Balance = Balance;
     };
--- src/modules/data/creditcarddata.ts
+++ src/modules/data/creditcarddata.ts
@@ -5,7 +5,7 @@ export class CreditCardData {
     expMonth: number | undefined;
     expYear: number | undefined;
     cvC2: number | undefined;
-    creditCardType: number | undefined;
+    //creditCardType: number | undefined;
     createdAt: Date | undefined;
     balance: number | undefined;
 }
